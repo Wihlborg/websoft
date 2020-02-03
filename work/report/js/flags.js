cd9d2e53844533e8c068d406487da2b523c36dd4
Henrik Vihlborg <henrik.vihlborg0017@stud.hkr.se>
2020-02-04 00:29:28 +0100

    Clicky flags

---
+++ work/report/js/flags.js
@@ -0,0 +1,11 @@
+document.getElementById("sweden").addEventListener('click', function(){
+    document.getElementById("swedenFlag").style.visibility = "visible";
+});
+
+document.getElementById("denmark").addEventListener('click', function(){
+    document.getElementById("denmarkFlag").style.visibility = "visible";
+});
+
+document.getElementById("france").addEventListener('click', function(){
+    document.getElementById("franceFlag").style.visibility = "visible";
+});
