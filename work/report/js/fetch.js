1d04badfaa6684aa65c9126bd52f7d70445a0f92
Henrik Vihlborg <henrik.vihlborg0017@stud.hkr.se>
2020-02-03 18:01:20 +0100

    Rewrote table code

--- work/report/js/fetch.js
+++ work/report/js/fetch.js
@@ -7,44 +7,34 @@ document.getElementById("fetchButton").addEventListener("click", function(){
         return response.json();
     })
     .then((myJson) => {
-        populateTable(myJson);
+        populateTable(myJson.Skolenheter);
     });
 });
 
-function populateTable(json){
-    var schools = json.Skolenheter;
-    var col = [];
+function populateTable(tableData){
+    var keys = [];
+    var table = document.getElementById('table');
+    var tablecontent = '';
+    tablecontent += ("<tr>");
 
-    for(var i = 0; i < schools.length; i++){
-        for(var key in schools[i]){
-            if(col.indexOf(key) === -1){
-                col.push(key);
-            }
+    for (key in tableData[0]) {
+
+        tablecontent += ('<th>' + key + '</th>');
+    }
+
+    tablecontent += ("</tr>");
+
+    for (var i = 0; i < tableData.length; i++) {
+
+        tablecontent += ("<tr>");
+
+        for (key in tableData[i]) {
+
+            tablecontent += ('<td>' + tableData[i][key] + '</td>');
         }
+
+        tablecontent += ("</tr>");
     }
 
-    var table = document.createElement("table");
-
-    var tr = table.insertRow(-1);
-
-    for (var i = 0; i < col.length; i++) {
-        var th = document.createElement("th");
-        th.innerHTML = col[i];
-        tr.appendChild(th);
-    }
-
-    for (var i = 0; i < schools.length; i++) {
-
-        tr = table.insertRow(-1);
-
-        for (var j = 0; j < col.length; j++) {
-            var tabCell = tr.insertCell(-1);
-            tabCell.innerHTML = schools[i][col[j]];
-        }
-    }
-
-    
-    var tableContainer = document.getElementById("tableContent");
-    tableContainer.innerHTML = "";
-    tableContainer.appendChild(table);
+    table.innerHTML = tablecontent;
 }
