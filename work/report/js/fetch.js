e1e1827c5a0dca01a20cfe6673f4b5de245c45ea
Henrik Vihlborg <henrik.vihlborg0017@stud.hkr.se>
2020-02-03 23:48:36 +0100

    Dropdown with json

--- work/report/js/fetch.js
+++ work/report/js/fetch.js
@@ -11,6 +11,34 @@ document.getElementById("fetchButton").addEventListener("click", function(){
     });
 });
 
+let dropdown = document.getElementById('locality-dropdown');
+dropdown.length = 0;
+
+let defaultOption = document.createElement('option');
+defaultOption.text = 'Choose Kommun-ID';
+
+dropdown.add(defaultOption);
+dropdown.selectedIndex = 0;
+
+const url = 'data/kommuner.json';
+
+fetch(url)  
+  .then(  
+    function(response) {  
+      // Examine the text in the response  
+      response.json().then(function(data) {  
+        let option;
+    
+    	for (let i = 0; i < data.length; i++) {
+          option = document.createElement('option');
+      	  option.text = data[i].name;
+      	  option.value = data[i].code;
+      	  dropdown.add(option);
+    	}    
+      });  
+    }  
+  )  
+
 function populateTable(tableData){
     var keys = [];
     var table = document.getElementById('table');
