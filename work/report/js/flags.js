39dca0ecfcd0757c535dc9ade155cd210ff101e6
Henrik Vihlborg <henrik.vihlborg0017@stud.hkr.se>
2020-02-04 01:07:34 +0100

    Transition 2.0

--- work/report/js/flags.js
+++ work/report/js/flags.js
@@ -1,11 +1,36 @@
+let swedenFlag = document.getElementById("swedenFlag");
+let denmarkFlag = document.getElementById("denmarkFlag");
+let franceFlag = document.getElementById("franceFlag");
+
 document.getElementById("sweden").addEventListener('click', function(){
-    document.querySelector("swedenFlag").className = "visible";
+    swedenFlag.style.transition = "2s";
+    swedenFlag.style.opacity = "1";
 });
 
 document.getElementById("denmark").addEventListener('click', function(){
-    document.querySelector("denmarkFlag").className = "visible";
+    denmarkFlag.style.transition = "2s";
+    denmarkFlag.style.opacity = "1";
 });
 
 document.getElementById("france").addEventListener('click', function(){
-    document.querySelector("franceFlag").className = "visible";
+    franceFlag.style.transition = "2s";
+    franceFlag.style.opacity = "1";
+});
+
+swedenFlag.addEventListener('click', function(){
+    if(swedenFlag.style.opacity !== 0){
+        swedenFlag.style.opacity = 0;
+    }
+});
+
+denmarkFlag.addEventListener('click', function(){
+    if(denmarkFlag.style.opacity !== 0){
+        denmarkFlag.style.opacity = 0;
+    }
+});
+
+franceFlag.addEventListener('click', function(){
+    if(franceFlag.style.opacity !== 0){
+        franceFlag.style.opacity = 0;
+    }
 });
