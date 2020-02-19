29060d0241b880b6627897b93e8ada902a2c0d52
Henrik Vihlborg <henrik.vihlborg0017@stud.hkr.se>
2020-02-19 04:56:46 +0100

    Prepared A5

---
+++ work/php/view/footer.php
@@ -0,0 +1,5 @@
+<footer class="site-footer">
+        <hr>
+        <p>Footers are cool</p>
+        <p><button id="duckButton">Hide duck</button></p>
+</footer>
---
+++ work/php/view/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title><?= $pageTitle ?></title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="favicon.ico">
+</head>
+
+<body>
+
+<header class="site-header">
+        <nav>
+            <a class="nav-a" href="me.php" alt="Me">Me</a> |
+            <a class="nav-a" href="report.php" alt="Report">Report</a> |
+            <a class="nav-a" href="about.php" alt="About">About</a> |
+            <a class="nav-a" href="school.php" alt="Schools">Schools</a> |
+            <a class="nav-a" href="flag.php" alt="Flags">Flags</a> |
+            <a class="nav-a" href="search.php" alt="Search">Search</a> |
+            <a class="nav-a" href="create.php" alt="Create">Create</a> |
+            <a class="nav-a" href="update.php" alt="Update">Update</a>
+        </nav>
+</header>
