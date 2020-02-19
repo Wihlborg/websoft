2012b2df75fa5d99f9771c584da9524dc1772694
Henrik Vihlborg <henrik.vihlborg0017@stud.hkr.se>
2020-02-19 05:42:25 +0100

    Header fix

--- work/php/report.php
+++ work/php/report.php
@@ -6,18 +6,6 @@
 
 <body>
 
-
-
-<header>
-    <nav>
-        <a href="me.html">Me</a> |
-        <a href="report.html">Report</a> | 
-        <a href="about.html">About</a> |
-        <a href="schools.html">Schools</a> |
-        <a href="flags.html">Flags</a>
-    </nav>
-</header>
-
 <hr>
 
 <article>
